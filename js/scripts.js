38f8bbf1b2611eef493b8b6bbda7594ad09b7e5b
Tiago Pinho <tiagoppinho@gmail.com>
2018-07-25 01:38:31 +0100

    Added basic functionality to side navigation bar.

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,15 @@ $(function(){
         e.preventDefault();
         $("html, body").animate({
             //gets the href attribute of the element and scrolls to their top.
-            scrollTop: $($(this).attr('href')).offset().top
+            scrollTop: $($(this).attr("href")).offset().top
         }, 1200);
     });
 })
 
-//TODO: 
-
-//TODO: 
-
+//Shows the side navigation bar or hides it, depending on the scroll position.
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= $("#about").offset().top - 5) 
+        $("#side-nav").css("visibility", "visible");
+    else
+        $("#side-nav").css("visibility", "hidden");
+});
