e36223b536f3ad10e4b2b0f75b6c34c83d3fe174
Tiago Pinho <tiagoppinho@gmail.com>
2018-07-26 13:32:53 +0100

    Added animation to side navigation bar.

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ $(window).scroll(function(){
         var windowTopPosition = $(window).scrollTop();
         $("#side-nav ul li a").not("first").each(function(){
             var section = $($(this).attr("href"));
-            var sectionTop = section.offset().top - 5;
+            var sectionTop = section.offset().top - 20;
             var sectionBottom = section.offset().top + section.height();
             if(sectionTop <= windowTopPosition && sectionBottom >= windowTopPosition){
                 //Removes any active anchor before setting up the new one.
